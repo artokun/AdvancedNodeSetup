c3c512c29f5c9d8dbee64ebb96efa4dfc8a27225
artokun <art.longbottom.jr@gmail.com>
2018-04-24 16:29:17 -0700

    added redis url

--- config/prod.js
+++ config/prod.js
@@ -3,6 +3,7 @@ module.exports = {
   googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
   mongoURI: process.env.MONGO_URI,
   cookieKey: process.env.COOKIE_KEY,
+  redisUrl: process.env.REDIS_URL,
   accessKeyId: process.env.S3_ACCESS_KEY_ID,
   secretAccessKey: process.env.S3_SECRET_ACCESS_KEY,
 };
