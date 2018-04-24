39d9bc0c5ab9d10f5585c71a5ad91460a195d391
artokun <art.longbottom.jr@gmail.com>
2018-04-24 16:49:48 -0700

    added example keys

---
+++ config/dev.example.js
@@ -0,0 +1,11 @@
+module.exports = {
+  googleClientID: '',
+  googleClientSecret: '',
+  mongoURI: '',
+  cookieKey: '',
+  redisUrl: '',
+  accessKeyId: '',
+  secretAccessKey: '',
+};
+
+// make sure to make new dev.js copy from this template (don't delete example!!)
